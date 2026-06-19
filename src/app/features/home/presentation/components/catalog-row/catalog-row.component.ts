4874938906e5cd525c959810b8ed07aa4a037a7d
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-06-19 17:36:47 +0000

    ajustado

--- src/app/features/home/presentation/components/catalog-row/catalog-row.component.ts
+++ src/app/features/home/presentation/components/catalog-row/catalog-row.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProjectCardComponent } from '@app/features/project-card/project-card.component';
+import { ProjectCardComponent } from '@app/features/home/presentation/components/project-card/project-card.component';
 import { ProjectModel } from '@app/core/models/project.model';
 
 @Component({
   selector: 'app-catalog-row',
   standalone: true,
-  imports: [CommonModule, ProjectCardComponent],
+  imports: [
+    CommonModule, 
+    ProjectCardComponent
+  ],
   templateUrl: './catalog-row.component.html',
   styleUrls: ['./catalog-row.component.scss'],
 })
